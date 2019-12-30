1bb2043a1c439c43b98ac43e9a7bdbe7b8ce667f
Yohann <breuil.yohann@orange.fr>
2019-12-30 20:15:30 +0100

    Update demandes.h

--- classes/demandes.h
+++ classes/demandes.h
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+#include "etudiants.h"
+#include "logements.h"
+
+/* Structures */
+typedef struct {
+	int idDemande;
+	int idEtudiant;
+	Echelon echelon;
+	char* nomCite;
+	TypeLogement typeLogement;
+} Demande;
+
+/* Fonctions */
