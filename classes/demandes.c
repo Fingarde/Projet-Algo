fa87b0e4bcf6c7a238f0a7e08b16d6299492b1d8
Yohann <breuil.yohann@orange.fr>
2019-12-30 20:28:14 +0100

    Update demandes.c

--- classes/demandes.c
+++ classes/demandes.c
@@ -5,4 +5,35 @@
 
 #include "demandes.h"
 
+char echelonValues[8][5] = {"0bis", "01", "02", "03", "04", "05", "06", "07"}; // Chaine de caractères correspondant aux valeurs de l'enum
+char* getEchelon(Echelon echelon) { // Retourne la chaine de caractères correspondant a la valeur de l'enum
+    return echelonValues[echelon];
+}
 
+char typeLogementValues[4][8] = {"Chambre", "Studio", "T1", "T2"}; // Chaine de caractères correspondant aux valeurs de l'enum
+char* getTypeLogement(TypeLogement typeLogement) { // Retourne la chaine de caractères correspondant a la valeur de l'enum
+    return typeLogementValues[typeLogement];
+}
+
+Demande lireDemande (FILE* flot) {
+	Demande dema;
+
+	char nomCite[65];
+    int tailleNomCite;
+
+	fscanf(flot, "%d%*c", &dema.idDemande);
+	fscanf(flot, "%d%*c", &dema.idEtudiant);
+
+	fgets(nomCite, 65, flot);
+    tailleNomCite = strlen(nomCite);
+    if(nomCite[tailleNomCite - 2] == '\n') {
+        nomCite[tailleNomCite - 2] = '\0';
+        tailleNomCite--;
+    }
+    dema.nomCite = (char*) malloc(sizeof(char) * tailleNomCite + 1);
+    if (dema.nomCite == NULL) {
+        printf("Problème d'allocation mémoire\n");
+        exit(1);
+    }
+    strcpy(dema.nomCite, nomCite);
+}
