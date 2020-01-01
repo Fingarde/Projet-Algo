0f87114a00e1afa8998b6dab8169d59e40259565
Yohann <breuil.yohann@orange.fr>
2020-01-01 12:30:26 +0100

    Update crous.c

--- crous.c
+++ crous.c
@@ -3,8 +3,4 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "classes/color.h"
-#include "classes/etudiants.h"
-#include "classes/logements.h"
-#include "classes/demandes.h"
 #include "crous.h"
