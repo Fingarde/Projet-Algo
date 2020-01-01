c8fb00f73a4bdc200e8f305263d3f2bfab3a7ea1
Yohann <breuil.yohann@orange.fr>
2020-01-01 15:16:20 +0100

    Update crous.c

--- crous.c
+++ crous.c
@@ -4,3 +4,14 @@
 #include <stdio.h>
 
 #include "crous.h"
+
+void affichMenu (void) {
+	printf("-- Bienvenue sur le programme de demandes de logement du CROUS --\n");
+	printf("1 - Afficher la liste des logements disponibles\n");
+	printf("2 - Afficher la liste des logements occupés\n");
+	printf("3 - Afficher la liste des demandes de logements en attente\n");
+	printf("4 - Saisir une nouvelle demande de logements\n");
+	printf("5 - Annuler une demande de logements\n");
+	printf("6 - Libérer un logement\n");
+	printf("7 - Quitter le programme\n");
+}
